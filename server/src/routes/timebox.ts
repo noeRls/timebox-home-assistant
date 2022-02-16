eec958027bab5e234080b70d68ba39b6d83efefe
Florian Baumann <derflob@derflob.de>
2022-02-16 11:53:36 +0100

    allow specifing display_type for time channel

--- server/src/routes/timebox.ts
+++ server/src/routes/timebox.ts
@@ -3,7 +3,7 @@ import { isLogged, validateMiddleware, withTimebox } from '../middleware';
 import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from 'http-status';
 import { IsBoolean, IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
 import * as multer from 'multer';
-import { BrightnessCommand, DisplayAnimation, DisplayText, TimeboxEvo, TimeChannel, DateTimeCommand } from 'node-divoom-timebox-evo';
+import { BrightnessCommand, DisplayAnimation, DisplayText, TimeboxEvo, TimeChannel, DateTimeCommand, TIMEBOX_CONST } from 'node-divoom-timebox-evo';
 
 const upload = multer({ dest: './images' });
 
@@ -63,7 +63,29 @@ router.post('/text', isLogged, validateMiddleware(TextBody), withTimebox(), asyn
 router.post('/time', isLogged, withTimebox(), async (req, res) => {
     try {
         const r = (new TimeboxEvo()).createRequest('time') as TimeChannel;
+
+        if (!req.body.display_type || req.body.display_type == "fullscreen") {
+            r.type = TIMEBOX_CONST.TimeType.FullScreen;
+        } else if (req.body.display_type == "rainbow") {
+            r.type = TIMEBOX_CONST.TimeType.Rainbow;
+        } else if (req.body.display_type == "with-box") {
+            r.type = TIMEBOX_CONST.TimeType.WithBox;
+        } else if (req.body.display_type == "analog-square") {
+            r.type = TIMEBOX_CONST.TimeType.AnalogSquare;
+        } else if (req.body.display_type == "analog-round") {
+            r.type = TIMEBOX_CONST.TimeType.AnalogRound;
+        } else if (req.body.display_type == "fullscreen-negative") {
+            r.type = TIMEBOX_CONST.TimeType.FullScreenNegative;
+        } else {
+            return res.status(BAD_REQUEST).send("Invalid time display type");
+        }
+
         await req.timebox.sendMultiple(r.messages.asBinaryBuffer());
+
+        const s = (new TimeboxEvo()).createRequest('raw');
+        s.push("2D01");
+        await req.timebox.sendMultiple(s.messages.asBinaryBuffer());
+
         return res.status(OK).end();
     } catch (e) {
         console.error(e);
